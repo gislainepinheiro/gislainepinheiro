e8c0f20b9c8ea4e39262d687db312380b719d399
renanm <renan@renanmedeiros.net>
2024-05-30 10:17:00 -0300

    Update funcoes.js

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,3 +1,6 @@
+/* A função abaixo é acionada quando o usuário toca no botão do menu nos dispositivos móveis.
+É o que faz aparecer e desaparecer a cada toque */
+
 function expandeMenu() {
     let menu = document.getElementById("nav-mobile");
     if (menu.style.display === "flex") {
