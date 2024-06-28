67f210e0f63989f198dd4958626eee94e86c802c
renanm <renan@renanmedeiros.net>
2024-06-28 16:08:45 -0300

    Update funcoes.js

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,3 +1,49 @@
+window.onload = inicio;
+
+function inicio() {
+    let hamburger = document.getElementsByClassName('hamburger-menu')[0];
+    hamburger.onclick = expandeMenu;
+
+    let botoes = document.getElementById('botoes').children;
+    if (botoes.length > 0) {
+        botoes[0].style.color = "#222";
+        for (let i = 0; i < botoes.length; i++) {
+            
+            botoes[i].onclick = function() {
+                mostrarServico(botoes[i].id.slice(6));
+            };
+            console.log(botoes[i].id);
+            console.log(botoes[i].onclick);
+        }
+    }
+}
+
+/* A função abaixo é responsável pela seleção dos serviços na página /servicos. Apenas nas telas maiores. */
+
+function mostrarServico(id) {
+    let botoes = document.getElementById('botoes').children;
+    console.log(botoes)
+    let conteudos = document.getElementsByClassName('conteudo');
+    for (let i = 0; i < botoes.length; i++) {
+        if (botoes[i].id === `botao-${id}`) {
+            botoes[i].classList.add("ativo");
+            botoes[i].style.color = "#222";
+        } else {
+            botoes[i].classList.remove("ativo");
+            botoes[i].style.color = "#fff";
+        }
+        console.log(conteudos[i].id); console.log(id);
+        if (conteudos[i].id === id) {
+            conteudos[i].classList.add("ativo");
+            conteudos[i].classList.remove("inativo");
+        } else {
+            conteudos[i].classList.remove("ativo");
+            conteudos[i].classList.add("inativo");
+        }
+    }
+}
+
+
 /* A função abaixo é acionada quando o usuário toca no botão do menu nos dispositivos móveis.
 É o que faz aparecer e desaparecer a cada toque */
 
